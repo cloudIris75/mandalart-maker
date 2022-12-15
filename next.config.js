e5e39c26587f7020e3890a1f6a3cc781ce1ce468
cloudIris75 <gmyeom1230@gmail.com>
2022-12-15 19:26:16 +0900

    Feat: 이미지 저장 버튼 구현

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ['mandalart-bucket.s3.ap-northeast-2.amazonaws.com', 'localhost'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,39 @@
+import html2canvas from 'html2canvas';
 import { NextPage } from 'next';
+import Image from 'next/image';
+import { useRef } from 'react';
 
 const Home: NextPage = () => {
+  const captureArea = useRef<HTMLDivElement>(null);
+
+  const onCaptureButtonClick = () => {
+    if (captureArea.current) {
+      html2canvas(captureArea.current).then(function (canvas) {
+        const element = document.createElement('a');
+        element.href = canvas.toDataURL('image/jpeg');
+        element.download = '2023_Mandalart.jpg';
+        element.click();
+      });
+    }
+  };
+
   return (
     <main className="p-4 h-screen flex flex-col items-center space-y-4">
       <h1 className="text-3xl">2023 만다라트 메이커</h1>
-      <div className="border border-black h-693 w-390 bg-background bg-cover"></div>
+      <div ref={captureArea} className="border border-black h-693 w-390">
+        <Image
+          src="https://mandalart-bucket.s3.ap-northeast-2.amazonaws.com/images/background.png"
+          alt="2023 Mandalart"
+          width={390}
+          height={693}
+        ></Image>
+      </div>
+      <button
+        onClick={onCaptureButtonClick}
+        className="border rounded-lg px-24 py-2"
+      >
+        이미지 저장
+      </button>
     </main>
   );
 };
