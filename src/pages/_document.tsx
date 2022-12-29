648a94f53ffb7258de5f09481ec4d8a421023865
cloudIris75 <gmyeom1230@gmail.com>
2022-12-29 23:29:07 +0900

    Feat: _document 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,27 @@ function MyDocument() {
       <Head>
         <link
           rel="icon"
-          href="https://mandalart-bucket.s3.ap-northeast-2.amazonaws.com/favicon/blackRabbit.png"
+          href="https://mandalart-bucket.s3.ap-northeast-2.amazonaws.com/favicon/2023mandalart.PNG"
+        />
+        <meta property="og:site_name" content="2023 만다라트 메이커" />
+        <meta property="og:title" content="2023 만다라트 메이커" />
+        <meta
+          property="og:description"
+          content="쉽고 빠른 새해 목표 세우기 🥕"
+        />
+        <meta
+          property="og:image"
+          content="https://mandalart-bucket.s3.ap-northeast-2.amazonaws.com/images/thumbnail.PNG"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://www.2023mandalart.swygbro.com"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          name="keywords"
+          content="만다라트, 계획표, 새해, 신년, 계획, 실천, 목표, 2023, Maldalart"
         />
         <meta name="viewport" content="width=device-width" />
       </Head>
